471a26d46cf06c497f41035523dc78d213bd5bd9
Your Name <wallicestenewaweru@gmail.com>
2023-06-14 17:20:33 +0300

    recipe page

--- src/components/RecipePage.jsx
+++ src/components/RecipePage.jsx
@@ -7,13 +7,7 @@ const RecipePage = () => {
     const { name } = useParams()
 
     const history = useHistory()
-    const ingerideientsArr = [1,2,3,4,5,6,7,8,9]
-    let ingName = 0
-    for(let i = 0; i < ingerideientsArr.length; i++ ){
-
-        ingName = ingerideientsArr[i]
-        console.log(ingName)
-    }
+   
 
     useEffect(() => {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
@@ -24,22 +18,44 @@ const RecipePage = () => {
         })
         .catch(err => console.log(err.message))
     }, [])
+// ingredients
+
+    const renderIngredients = () => {
+        const ingredients = [];
+        for (let i = 1; i <= 20; i++) {
+          const ingredient = recipe[`strIngredient${i}`];
+          const measure = recipe[`strMeasure${i}`];
+          const ingredientImageSrc = `https://www.themealdb.com/images/ingredients/${ingredient}.png`;
+          if (ingredient && measure) {
+            ingredients.push(
+              <li key={i} className=' w-32 h-full border rounded-2xl bg-slate-800 shadow-2xl font-LosefinSans'>
+                <div className='h-10 w-20 relative top-0'>
+                     <p className='text-xs text-white p-2 inline-block'>{measure} {ingredient}</p>
+                </div>
+                <img src={ingredientImageSrc} alt="" className='h-24 w-full object-contain opacity-75'/>
+              </li>
+            );
+          }
+        }
+        return ingredients;
+      };
 
   return (
-    <div className='recipe-page relative bg-slate-400'>
+    <div className='recipe-page relative my-5 text-slate-800'>
         <div className='grid lg:grid-cols-2 md:grid-cols-2 gap-3 lg:gap-5 grid-cols-1'>
-        <div className='left lg:w-full grid grid-cols-1 gap-2'>
-            <img src={recipe.strMealThumb} alt="" className='w-full lg:h-72 lg:object-cover opacity-90'/>
-            <div className='absolute top-3 left-4 text-white' onClick={() => history.go(-1)}>
+        <div className='left lg:w-full grid grid-cols-1 lg:gap-5 lg:flex flex-col gap-3'>
+            <img src={recipe.strMealThumb} alt="" className='w-full lg:h-72 lg:object-cover opacity-90 rounded-lg'/>
+            <div className='absolute top-3 left-4 bg-slate-800 text-white rounded-md p-1 hover:cursor-pointer' onClick={() => history.go(-1)}>
              <KeyboardBackspace fontSize='large'/>   
             </div>
-            <div>
-            <h2>Ingredients</h2>
-            <img src={`https://www.themealdb.com/images/ingredients/${recipe.strIngredient6}-Small.png`} alt="" />
+            <div className='shadow-2xl'>
+            <h2 className='text-3xl font-Lora font-bold mb-5 p-3'>Ingredients</h2>
+            <ul className='flex overflow-scroll gap-5 p-5 scroll-smooth'>{renderIngredients()}</ul>
            </div>
         </div>
-        <div>
-           <p>
+        <div className='rounded-lg border p-2 shadow-inner'>
+            <h1 className='text-3xl font-Lora font-bold mb-5'>Instructions</h1>
+           <p className=' font-LosefinSans lg:text-lg'>
             {recipe.strInstructions}
            </p>
         </div>
