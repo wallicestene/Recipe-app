0828140ad8ae54035bc140b6c232f72d3ad44a5e
Your Name <wallicestenewaweru@gmail.com>
2023-06-22 18:18:24 +0300

    profile page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { useFavourite } from './DataLayer'
 import { onAuthStateChanged, signOut } from 'firebase/auth'
 import { auth } from '../Firebase'
 import logo from "./logo/logo.png"
-import { Avatar } from '@mui/material'
+
 import { Link } from 'react-router-dom/cjs/react-router-dom'
 import { KeyboardArrowDown, KeyboardArrowUp, Logout } from '@mui/icons-material'
 
@@ -23,7 +23,6 @@ const Navbar = () => {
   const [showLogOut, setShowLogOut] = useState(false)
 
   const [{user}, dispatch] = useFavourite()
-
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (!user) {
@@ -56,15 +55,15 @@ const Navbar = () => {
           <div onClick={() => setShowLogOut(!showLogOut) } className=' cursor-pointer' >
             <h1 className=' text-sm text-gray-500 font-LosefinSans first-letter:uppercase'>{user.displayName ? user.displayName : "Profile"}{showLogOut ? <KeyboardArrowUp fontSize='small'/> : <KeyboardArrowDown fontSize='small'/>}</h1>
           </div>
-           <div className='h-10 w-10 bg-gradient-to-r from-gray-300 to-slate-900  rounded-full overflow-hidden grid place-items-center text-white cursor-pointer hover:border-2 border-rose-500'>
-            {!user?.photoURL ? (<div  className=' w-full h-full flex items-center justify-center font-LosefinSans text-3xl p-1 '>{user?.displayName[0].toUpperCase()}</div>) : isURL(user?.photoURL) ? (<img src={user?.photoURL} alt="" className='w-full h-full'/>): (<div className=' w-full h-full flex items-center justify-center font-LosefinSans text-3xl p-1 '>{user?.displayName[0].toUpperCase()}</div> )}
+           <div className='h-10 w-10 bg-gradient-to-r from-gray-300 to-slate-900  rounded-full overflow-hidden grid place-items-center text-white border-rose-500'>
+           {!user?.photoURL ? (<div  className=' w-full h-full flex items-center justify-center font-poppins text-3xl p-1 '>{user?.displayName ? user.displayName[0].toUpperCase() : ''}</div>) : isURL(user?.photoURL) ? (<img src={user?.photoURL} alt="" className='w-full h-full'/>): (<div className=' w-full h-full flex items-center justify-center font-poppins font-bold text-3xl p-1 '>{user?.displayName ? user.displayName[0].toUpperCase() : ''}</div> )}
           </div>
         </div>
-      <div className={` ${!showLogOut ? " hidden" : ""} absolute shadow-lg z-50 top-10 bg-gradient-to-r from-gray-700 to-slate-500 rounded-md  -left-20 lg:-left-20 border-2 text-white p-1`} >
+      <div className={` ${!showLogOut ? " hidden" : ""} absolute z-50 top-11 bg-gradient-to-r from-gray-700 to-slate-500 rounded-md  -left-20 lg:-left-24 border-2 text-white p-1 shadow-xl`} >
         <div>
      <div className=' w-fit flex gap-2 p-1 border-b-2'>
-      <div className=' w-14 h-14 rounded-full overflow-hidden'>
-      {!user?.photoURL ? (<div  className=' w-full h-full flex items-center justify-center font-LosefinSans text-3xl p-1'>{user?.displayName[0].toUpperCase()}</div>) : isURL(user?.photoURL) ? (<img src={user?.photoURL} alt="" className='w-full h-full'/>): (<div className=' w-full h-full flex items-center justify-center font-LosefinSans text-3xl p-1'>{user?.displayName[0].toUpperCase()}</div> )}
+      <div className=' w-14 h-14 rounded-full overflow-hidden bg-gradient-to-r from-gray-300 to-slate-900 '>
+      {!user?.photoURL ? (<div  className=' w-full h-full flex items-center justify-center font-poppins text-3xl p-1 '>{user?.displayName ? user.displayName[0].toUpperCase() : ''}</div>) : isURL(user?.photoURL) ? (<img src={user?.photoURL} alt="" className='w-full h-full'/>): (<div className=' w-full h-full flex items-center justify-center font-poppins font-bold text-3xl p-1 '>{user?.displayName ? user.displayName[0].toUpperCase() : ''}</div> )}
       </div>
      <div className=' grid place-items-center'>
      <p className="text-sm first-letter:uppercase">{user.displayName}</p>
@@ -72,7 +71,7 @@ const Navbar = () => {
      </div>
      </div>
      <div className='lg:p-3 p-2 grid place-items-center' >
-      <h1 className='cursor-pointer inline border p-1 rounded-md text-xs' onClick={() => signOut(auth)}>Log Out <Logout fontSize='small'/></h1>
+      <h1 className='cursor-pointer inline border p-1 rounded-md text-xs transition ease-in-out delay-100  hover:-translate-x-1 hover:scale-90 duration-200' onClick={() => signOut(auth)}>Log Out <Logout fontSize='small'/></h1>
      </div>
      </div>
     </div>
